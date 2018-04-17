f31ef97644480fe7a3e932a6fb0321a5de8c530e
Bob Costas <finkzane@gmail.com>
2018-04-16 19:15:30 -0700

    Added code given by michael

--- AdjacentNode.java
+++ AdjacentNode.java
@@ -3,4 +3,53 @@ package p11_Package;
 public class AdjacentNode
 {
 
+    /**
+     * character vertex letter
+     */
+    private char vertexLetter;
+
+    /**
+     * integer vertex weight
+     */
+    private int vertexWeight;
+
+    /**
+     * Initialization constructor
+     * @param vertex character vertex letter
+     * @param weight integer vertex weight
+     */
+    public AdjacentNode( char vertex, int weight )
+    {
+        vertexLetter = vertex;
+        vertexWeight = weight;
+    }
+
+    /**
+     * Copy constructor
+     * @param copied AdjacentNode item to be copied
+     */
+    public AdjacentNode( AdjacentNode copied )
+    {
+        vertexWeight = copied.vertexWeight;
+
+        vertexWeight = copied.vertexWeight;
+    }
+
+    /**
+     * Gets vertex letter
+     * @return character vertex letter
+     */
+    public char getVertex()
+    {
+        return vertexLetter;
+    }
+
+    /**
+     * Gets vertex weight
+     * @return integer vertex weight
+     */
+    public int getWeight()
+    {
+       return vertexWeight;
+    }
 }
