f3ee81512693056a7c3a2a1232e08a083bea3468
Bob Costas <finkzane@gmail.com>
2018-04-17 00:48:48 -0700

    Initial commit

---
+++ VertexNode.java
@@ -0,0 +1,12 @@
+package p11_Package;
+
+/**
+ * Simple vertex class that holds a vertex, its adjacent vertices,
+ * and the weight between them
+ *
+ * @author MichaelL
+ */
+public class VertexNode
+{
+
+}
