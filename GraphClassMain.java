48f1772b1adb9c8be0d69db176448fde22e2acb7
Bob Costas <finkzane@gmail.com>
2018-04-23 11:30:01 -0700

    Added nodes to test BFS/DFS

--- GraphClassMain.java
+++ GraphClassMain.java
@@ -1,4 +1,20 @@
 package p11_Package;
 
-public class GraphClassMain {
+public class GraphClassMain
+{
+    public static void main( String[] args )
+    {
+        GraphClass gc = new GraphClass();
+        gc.setVertex( 'A', 'D', 1 );
+        gc.setVertex( 'A', 'B', 1 );
+        gc.setVertex( 'D', 'C', 1 );
+        gc.setVertex( 'C', 'R', 1 );
+        gc.setVertex( 'B', 'C', 1 );
+        gc.setVertex( 'R', 'O', 1 );
+        gc.setVertex( 'B', 'F', 1 );
+
+        gc.generateAdjacencyMatrix();
+
+        System.out.println( gc.BFS( 'A', true ) );
+    }
 }
